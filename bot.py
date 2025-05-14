a769c443c5b62635a07e73de92c123fe0973cd61
borisjosesaldarrega <63525292+borisjosesaldarrega@users.noreply.github.com>
2025-05-14 00:44:27 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,21 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 from keep_alive import keep_alive
 import aiohttp
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 # Configuración inicial
 load_dotenv()
