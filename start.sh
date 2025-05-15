2d7d2cbee4e9e98eae7b092353f2a0422a877017
borisjosesaldarrega <63525292+borisjosesaldarrega@users.noreply.github.com>
2025-05-15 14:30:46 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# Verifica FFmpeg
+if ! command -v ffmpeg &> /dev/null; then
+    echo "❌ Error: FFmpeg no está instalado. Usa Docker o contacta a Render.";
+    exit 1;
+fi
+# Inicia el bot
 python bot.py
